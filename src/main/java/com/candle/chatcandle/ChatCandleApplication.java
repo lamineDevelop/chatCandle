0919fe14f77ed797f12acb37030f937ffa9ac936
lamine1305 <lamine.mehidi01@gmail.com>
2023-01-22 12:41:09 +0100

    init projet

---
+++ src/main/java/com/candle/chatcandle/ChatCandleApplication.java
@@ -0,0 +1,13 @@
+package com.candle.chatcandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatCandleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/candle/chatcandle/ChatCandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candle.chatcandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatCandleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
